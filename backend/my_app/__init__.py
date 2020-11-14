0c661ff84590fc8be5e35498df6f7c22063753ac
Louis Lee <pandawithcat@gmail.com>
2020-11-14 22:00:50 +0900

    fixed some bugs

--- backend/my_app/__init__.py
+++ backend/my_app/__init__.py
@@ -1,23 +1,32 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql.schema import Column, ForeignKey, MetaData, Table
+from sqlalchemy.types import Integer, String
+from sqlalchemy.engine import create_engine
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/mini_amazon'
+addr = 'mysql://root@localhost/mini_amazon'
+app.config['SQLALCHEMY_DATABASE_URI'] = addr
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # this config should be later changed
 # skip username and password etc, just use root cuz it's not productio lol
+
 db = SQLAlchemy(app)
+
+from my_app.user.models import User
+from my_app.item.models import Item
+from my_app.cart.models import Cart
+
 db.create_all()
 
-# TODO write some scripts for adding new data
-# ex) feel free to use item/models.py for reference. 
+# Some scripts for adding initial data
 from my_app.item.models import Item
 import csv
 with open('amazon_data.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        print(row)
         url = row['product_url']
         name = row['product_name']
+        name = name[0:150]
         price = row['price']
         category = row['category']
         image = row['image']
@@ -25,6 +34,8 @@ with open('amazon_data.csv', newline='') as csvfile:
         brand = row['brand']
         new_item = Item(url, name, price, category, image, description, brand)
         db.session.add(new_item)
+        db.session.commit()
+        print("this is the added item name and number: ", name, new_item)
 
 from my_app.user.views import user_blueprint    
 app.register_blueprint(user_blueprint)
